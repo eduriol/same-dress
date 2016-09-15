08fd2b06d45fcebf665e8fe3220a0b8a555e75ac
eduriol <eduriol@gmail.com>
2016-09-15 18:28:05 +0200

    Tests finished for OutfitButtonToolbar component

--- imports/ui/OutfitButtonToolbar.tests.jsx
+++ imports/ui/OutfitButtonToolbar.tests.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
+import { Random } from 'meteor/random';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import { Button, ButtonToolbar, Glyphicon } from 'react-bootstrap';
 
 import OutfitButtonToolbar from './OutfitButtonToolbar';
+import { Outfits, removeOutfit } from '../api/outfits.js';
 
 describe("OutfitButtonToolbar", function() {
-  it("contains one ButtonToolBar", function() {
-    const wrapper = shallow(<OutfitButtonToolbar />).find(ButtonToolbar);
+	const userId = Random.id();
+  const outfitId = Outfits.insert({
+			type: 'short',
+			brand: 'Chanel',
+			color: 'pink',
+			event: 'wedding',
+			owner: userId,
+    });
+  outfit = Outfits.findOne({ _id: outfitId });
+
+  it("contains a table cell", function() {
+    const wrapper = shallow(<OutfitButtonToolbar outfit={outfit} />).find('td');
     expect(wrapper).to.have.length(1);
-	});  
+	});
+
+  it("contains one ButtonToolBar", function() {
+    const wrapper = shallow(<OutfitButtonToolbar outfit={outfit} />).find('td').find(ButtonToolbar);
+    expect(wrapper).to.have.length(1);
+	});
 
   it("ButtonToolBar contains two buttons", function() {
-  	const wrapper = shallow(<OutfitButtonToolbar />).find(ButtonToolbar).children().find(Button);
+  	const wrapper = shallow(<OutfitButtonToolbar outfit={outfit} />).find('td').find(ButtonToolbar).children().find(Button);
   	expect(wrapper).to.have.length(2);
 	});
 
 	it("Both buttons contain Glyphicons", function() {
-  	const wrapper = shallow(<OutfitButtonToolbar />).find(ButtonToolbar).children().find(Button);
-  	expect(wrapper.contains([
-  		<Glyphicon glyph="pencil" />,
-  		<Glyphicon glyph="remove" />,
-		])).to.equal(true);
+  	const wrapper = shallow(<OutfitButtonToolbar outfit={outfit} />).find('td').find(ButtonToolbar).children().find(Button);
+  	expect(wrapper.at(0).contains(<Glyphicon glyph="pencil" />)).to.equal(true);
+  	expect(wrapper.at(1).contains(<Glyphicon glyph="remove" />)).to.equal(true);
 	});
 
 });

adb109bb84b839669e544f71cef839da014be192
eduriol <eduriol@gmail.com>
2016-10-26 09:52:03 +0200

    First FB API example

--- imports/ui/EventsList.jsx
+++ imports/ui/EventsList.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { Meteor } from 'meteor/meteor';
 
 export default class EventsList extends Component {
 
+  renderFacebookUser() {
+    /* make the API call */
+    FB.api(
+        "/10154740868438578?fields=email",
+        {
+          access_token : 'EAAZAKXj1Tfa8BAJAGnhn73hj36dZBZC6bG9gX42uUdikeg0snCY0z8EH0mFdasbI6VAFSItZBv0dBIQwkuKMhhQeX280EOBhKb4NP9rFmJyCjXcKg7ZAEC9oAABKsrZCkEpOvsMIchcghujZB4SYEjZAJFkrX0ecxksZD'
+        },
+        function (response) {
+          if (response && !response.error) {
+            alert(JSON.stringify(response));
+          }
+        }
+    );
+  }
+
   render() {
-    //return this.props.events;
-    return 'HOLA';
+    return (
+      <div>
+        {this.renderFacebookUser()}
+      </div>
+    );
   }
 
 }
